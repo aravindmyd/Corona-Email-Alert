bca84b910f6d70700b9a5ebeb6e1a4a4da4366b4
Aravind Naveen <38346034+aravindmyd@users.noreply.github.com>
2020-04-02 11:57:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-import main as client_app
+import app as client_app
 import mail
 
 app = Flask(__name__)
